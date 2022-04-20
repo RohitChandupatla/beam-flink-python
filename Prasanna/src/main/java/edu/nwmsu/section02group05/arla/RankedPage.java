ae29973d1717ef18def7dd066a4d5afb215ba6e9
PRASANNAARLA <S542294@nwmissouri.edu>
2022-04-19 21:07:45 -0500

    added doFn

--- Prasanna/src/main/java/edu/nwmsu/section02group05/arla/RankedPage.java
+++ Prasanna/src/main/java/edu/nwmsu/section02group05/arla/RankedPage.java
@@ -1,10 +1,26 @@
 package edu.nwmsu.section02group05.arla;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.apache.beam.runners.core.construction.resources.PipelineResourcesOptions;
+import org.apache.beam.sdk.Pipeline;
+import org.apache.beam.sdk.io.TextIO;
+import org.apache.beam.sdk.options.PipelineOptions;
+import org.apache.beam.sdk.options.PipelineOptionsFactory;
+import org.apache.beam.sdk.transforms.Filter;
+import org.apache.beam.sdk.transforms.FlatMapElements;
+import org.apache.beam.sdk.transforms.Flatten;
+import org.apache.beam.sdk.transforms.GroupByKey;
+import org.apache.beam.sdk.transforms.MapElements;
+import org.apache.beam.sdk.values.KV;
+import org.apache.beam.sdk.values.PCollection;
+import org.apache.beam.sdk.values.PCollectionList;
+import org.apache.beam.sdk.values.TypeDescriptors;
 
 public class RankedPage {
-
     public RankedPage(String key, ArrayList<VotingPage> voters) {
     }
-
+    String name = "example.md";
+    Double rank = 1.000;
 }
