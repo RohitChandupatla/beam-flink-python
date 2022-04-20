15167aa462eabd78326b343bdceeaebdd55127f8
PRASANNAARLA <S542294@nwmissouri.edu>
2022-04-19 20:28:44 -0500

    changed the rankedpage.java

--- Prasanna/src/main/java/edu/nwmsu/section02group05/arla/RankedPage.java
+++ Prasanna/src/main/java/edu/nwmsu/section02group05/arla/RankedPage.java
@@ -1,5 +1,10 @@
 package edu.nwmsu.section02group05.arla;
 
+import java.util.ArrayList;
+
 public class RankedPage {
-    
+
+    public RankedPage(String key, ArrayList<VotingPage> voters) {
+    }
+
 }
