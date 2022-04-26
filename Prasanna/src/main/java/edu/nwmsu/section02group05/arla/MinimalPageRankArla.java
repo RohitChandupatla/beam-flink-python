40f548713c1ceedbb295b78939b73a2558a872d4
PRASANNAARLA <S542294@nwmissouri.edu>
2022-04-26 18:11:05 -0500

    updated the minimal page rank , votingpage, rankedpage

--- Prasanna/src/main/java/edu/nwmsu/section02group05/arla/MinimalPageRankArla.java
+++ Prasanna/src/main/java/edu/nwmsu/section02group05/arla/MinimalPageRankArla.java
@@ -35,25 +35,24 @@ import java.util.ArrayList;
 //     - IO
 //     - Core Transforms
 
-import java.util.Arrays;
+
 import java.util.Collection;
 
-//import org.apache.beam.model.pipeline.v1.RunnerApi.PCollection;
 import org.apache.beam.sdk.Pipeline;
 import org.apache.beam.sdk.io.TextIO;
 import org.apache.beam.sdk.options.PipelineOptions;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
-import org.apache.beam.sdk.transforms.Count;
 import org.apache.beam.sdk.transforms.DoFn;
 import org.apache.beam.sdk.transforms.Filter;
-import org.apache.beam.sdk.transforms.FlatMapElements;
 import org.apache.beam.sdk.transforms.Flatten;
+import org.apache.beam.sdk.transforms.GroupByKey;
 import org.apache.beam.sdk.transforms.MapElements;
+import org.apache.beam.sdk.transforms.ParDo;
 import org.apache.beam.sdk.values.KV;
 import org.apache.beam.sdk.values.PCollection;
 import org.apache.beam.sdk.values.PCollectionList;
-import org.apache.beam.sdk.values.TypeDescriptor;
 import org.apache.beam.sdk.values.TypeDescriptors;
+
 
 public class MinimalPageRankArla {
 
@@ -74,6 +73,50 @@ public class MinimalPageRankArla {
       receiver.output(KV.of(element.getKey(), new RankedPage(element.getKey(), voters)));
     }
   }
+  static class Job2Mapper extends DoFn<KV<String, RankedPage>, KV<String, RankedPage>> {
+    @ProcessElement
+    public void processElement(@Element KV<String, RankedPage> element,
+      OutputReceiver<KV<String, RankedPage>> receiver) {
+      int votes = 0;
+      ArrayList<VotingPage> voters = element.getValue().getVoterList();
+      if(voters instanceof Collection){
+        votes = ((Collection<VotingPage>) voters).size();
+      }
+      for(VotingPage vp: voters){
+        String pageName = vp.getVoterName();
+        double pageRank = vp.getPageRank();
+        String contributingPageName = element.getKey();
+        double contributingPageRank = element.getValue().getRank();
+        VotingPage contributor = new VotingPage(contributingPageName,votes,contributingPageRank);
+        ArrayList<VotingPage> arr = new ArrayList<>();
+        arr.add(contributor);
+        receiver.output(KV.of(vp.getVoterName(), new RankedPage(pageName, pageRank, arr)));        
+      }
+    }
+  }
+
+  static class Job2Updater extends DoFn<KV<String, Iterable<RankedPage>>, KV<String, RankedPage>> {
+    @ProcessElement
+    public void processElement(@Element KV<String, Iterable<RankedPage>> element,
+      OutputReceiver<KV<String, RankedPage>> receiver) {
+        Double dampingFactor = 0.85;
+        Double updatedRank = (1 - dampingFactor);
+        ArrayList<VotingPage> newVoters = new ArrayList<>();
+        for(RankedPage rankPage:element.getValue()){
+          if (rankPage != null) {
+            for(VotingPage votingPage:rankPage.getVoterList()){
+              newVoters.add(votingPage);
+              updatedRank += (dampingFactor) * votingPage.getPageRank() / (double)votingPage.getContributorVotes();
+              // newVoters.add(new VotingPage(votingPage.getVoterName(),votingPage.getContributorVotes(),updatedRank));
+            }
+          }
+        }
+        receiver.output(KV.of(element.getKey(),new RankedPage(element.getKey(), updatedRank, newVoters)));
+
+    }
+
+  }
+  
   
   
 
@@ -103,11 +146,31 @@ public class MinimalPageRankArla {
 
     PCollection<KV<String, String>> arlaMergedList = pcKVpairs.apply(Flatten.<KV<String,String>>pCollections());
 
-    PCollection<String> PCollLinksString =  arlaMergedList.apply(
-      MapElements.into(  
-        TypeDescriptors.strings())
-          .via((myMergeLstout) -> myMergeLstout.toString()));
+    PCollection<KV<String, Iterable<String>>> pCollectionGroupByKey = arlaMergedList.apply(GroupByKey.create());
+    // Convert to a custom Value object (RankedPage) in preparation for Job 2
+    PCollection<KV<String, RankedPage>> job02Input = pCollectionGroupByKey.apply(ParDo.of(new Job1Finalizer()));
+  
+    PCollection<KV<String,RankedPage>> job2Mapper = job02Input.apply(ParDo.of(new Job2Mapper()));
+  
 
+  PCollection<KV<String, RankedPage>> job02Output = null; 
+  PCollection<KV<String,Iterable<RankedPage>>> job02MapperGroupbkey = job2Mapper.apply(GroupByKey.create());
+    
+  job02Output = job02MapperGroupbkey.apply(ParDo.of(new Job2Updater()));
+
+  
+  job02MapperGroupbkey = job02Output.apply(GroupByKey.create());
+    
+  job02Output = job02MapperGroupbkey.apply(ParDo.of(new Job2Updater()));
+  
+  job02Output = job02MapperGroupbkey.apply(ParDo.of(new Job2Updater()));
+  job02MapperGroupbkey = job02Output.apply(GroupByKey.create());    
+  job02Output = job02MapperGroupbkey.apply(ParDo.of(new Job2Updater()));   
+
+    PCollection<String> PCollLinksString =  job02Output.apply(
+      MapElements.into(
+            TypeDescriptors.strings()).via(
+                kvtoString -> kvtoString.toString()));
        
         //
         // By default, it will write to a set of files with names like wordcounts-00001-of-00005
--- Prasanna/src/main/java/edu/nwmsu/section02group05/arla/VotingPage.java
+++ Prasanna/src/main/java/edu/nwmsu/section02group05/arla/VotingPage.java
@@ -1,9 +1,49 @@
 package edu.nwmsu.section02group05.arla;
 
-public class VotingPage {
-    public VotingPage(String voterName, Integer contributorVotes) {
+import java.io.Serializable;
+
+import org.apache.beam.sdk.transforms.DoFn;
+import org.apache.beam.sdk.values.KV;
+
+public  class VotingPage extends DoFn<KV<String,Iterable<String>>,KV<String,RankedPage>> implements Serializable{
+    String voterName;
+    int contributorVotes;
+    double pageRank;
+    public VotingPage(String voterName,Integer contributorVotes2, double pageRank){
+        this.voterName = voterName;
+        this.contributorVotes = contributorVotes2;      
+        this.pageRank = pageRank;  
     }
-    String name = "test.md";
-    Double rank = 1.000;
-    int voteCount = 0;
+
+    public VotingPage(String voterName,Integer contributorVotes2){
+        this.voterName = voterName;
+        this.contributorVotes = contributorVotes2;      
+        this.pageRank = 1.0;  
+    }
+    
+    public String getVoterName() {
+        return voterName;
+    }
+    public void setVoterName(String voterName) {
+        this.voterName = voterName;
+    }
+    public int getContributorVotes() {
+        return contributorVotes;
+    }
+    public void setContributorVotes(int contributorVotes) {
+        this.contributorVotes = contributorVotes;
+    }
+    @Override
+    public String toString() {
+        return "voterName = "+ voterName +", Page rank = "+this.pageRank +" ContributorVotes = " + contributorVotes;
+    }
+
+    public double getPageRank() {
+        return this.pageRank;
+    }
+    public void setPageRank(double pageRank){
+        this.pageRank = pageRank;
+    }
+
+
 }
