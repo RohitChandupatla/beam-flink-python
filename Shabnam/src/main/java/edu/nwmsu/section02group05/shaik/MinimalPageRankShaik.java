5b29a1fd1d7822c707cd2e773ecc34e86bc36f6f
S542362 <77766701+ShabnamShaikk@users.noreply.github.com>
2022-04-26 17:21:46 -0500

    updated

--- Shabnam/src/main/java/edu/nwmsu/section02group05/shaik/MinimalPageRankShaik.java
+++ Shabnam/src/main/java/edu/nwmsu/section02group05/shaik/MinimalPageRankShaik.java
@@ -2,20 +2,26 @@
  package edu.nwmsu.section02group05.shaik;
 
 
-
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
+
 import org.apache.beam.sdk.Pipeline;
 import org.apache.beam.sdk.io.TextIO;
 import org.apache.beam.sdk.options.PipelineOptions;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
 import org.apache.beam.sdk.transforms.Count;
+import org.apache.beam.sdk.transforms.DoFn;
 import org.apache.beam.sdk.transforms.Filter;
 import org.apache.beam.sdk.transforms.FlatMapElements;
 import org.apache.beam.sdk.transforms.Flatten;
+import org.apache.beam.sdk.transforms.GroupByKey;
 import org.apache.beam.sdk.transforms.MapElements;
+import org.apache.beam.sdk.transforms.ParDo;
 import org.apache.beam.sdk.values.KV;
 import org.apache.beam.sdk.values.PCollection;
 import org.apache.beam.sdk.values.PCollectionList;
+import org.apache.beam.sdk.values.PCollectionTuple;
 import org.apache.beam.sdk.values.TypeDescriptor;
 import org.apache.beam.sdk.values.TypeDescriptors;
 
@@ -31,30 +37,75 @@ public class MinimalPageRankShaik {
 
   /**
    * DoFn Job1Finalizer takes KV(String, String List of outlinks) and transforms
-   * the value into our custom RankedPage Value holding the page's rank and list
+   * the value into our custom RankedPageShaik Value holding the page's rank and list
    * of voters.
    * 
    * The output of the Job1 Finalizer creates the initial input into our
    * iterative Job 2.
    */
-  static class Job1Finalizer extends DoFn<KV<String, Iterable<String>>, KV<String, RankedPage>> {
+  static class Job1Finalizer extends DoFn<KV<String, Iterable<String>>, KV<String, RankedPageShaik>> {
     @ProcessElement
     public void processElement(@Element KV<String, Iterable<String>> element,
-        OutputReceiver<KV<String, RankedPage>> receiver) {
+        OutputReceiver<KV<String, RankedPageShaik>> receiver) {
       Integer contributorVotes = 0;
       if (element.getValue() instanceof Collection) {
         contributorVotes = ((Collection<String>) element.getValue()).size();
       }
-      ArrayList<VotingPage> voters = new ArrayList<VotingPage>();
+      ArrayList<VotingPageShaik> voters = new ArrayList<VotingPageShaik>();
       for (String voterName : element.getValue()) {
         if (!voterName.isEmpty()) {
-          voters.add(new VotingPage(voterName, contributorVotes));
+          voters.add(new VotingPageShaik(voterName, contributorVotes));
         }
       }
-      receiver.output(KV.of(element.getKey(), new RankedPage(element.getKey(), voters)));
+      receiver.output(KV.of(element.getKey(), new RankedPageShaik(element.getKey(), voters)));
     }
   }
 
+
+  static class Job2Mapper extends DoFn<KV<String, RankedPageShaik>, KV<String, RankedPageShaik>> {
+    @ProcessElement
+    public void processElement(@Element KV<String, RankedPageShaik> element,
+      OutputReceiver<KV<String, RankedPageShaik>> receiver) {
+      int votes = 0;
+      ArrayList<VotingPageShaik> voters = element.getValue().getVoterList();
+      if(voters instanceof Collection){
+        votes = ((Collection<VotingPageShaik>) voters).size();
+      }
+      for(VotingPageShaik vp: voters){
+        String pageName = vp.getVoterName();
+        double pageRank = vp.getPageRank();
+        String contributingPageName = element.getKey();
+        double contributingPageRank = element.getValue().getRank();
+        VotingPageShaik contributor = new VotingPageShaik(contributingPageName,votes,contributingPageRank);
+        ArrayList<VotingPageShaik> arr = new ArrayList<>();
+        arr.add(contributor);
+        receiver.output(KV.of(vp.getVoterName(), new RankedPageShaik(pageName, pageRank, arr)));        
+      }
+    }
+  }
+
+  static class Job2Updater extends DoFn<KV<String, Iterable<RankedPageShaik>>, KV<String, RankedPageShaik>> {
+    @ProcessElement
+    public void processElement(@Element KV<String, Iterable<RankedPageShaik>> element,
+      OutputReceiver<KV<String, RankedPageShaik>> receiver) {
+        Double dampingFactor = 0.85;
+        Double updatedRank = (1 - dampingFactor);
+        ArrayList<VotingPageShaik> newVoters = new ArrayList<>();
+        for(RankedPageShaik rankPage:element.getValue()){
+          if (rankPage != null) {
+            for(VotingPageShaik votingPage:rankPage.getVoterList()){
+              newVoters.add(votingPage);
+              updatedRank += (dampingFactor) * votingPage.getPageRank() / (double)votingPage.getContributorVotes();
+            }
+          }
+        }
+        receiver.output(KV.of(element.getKey(),new RankedPageShaik(element.getKey(), updatedRank, newVoters)));
+
+    }
+
+  }
+
+  
   public static void main(String[] args) {
 
    
@@ -78,11 +129,31 @@ public class MinimalPageRankShaik {
     PCollectionList<KV<String, String>> keyvalpairList = PCollectionList.of(keyvalpair01).and(keyvalpair02).and(keyvalpair03).and(keyvalpair04);
 
     PCollection<KV<String, String>> MergedListShaik = keyvalpairList.apply(Flatten.<KV<String,String>>pCollections());
+    PCollection<KV<String, Iterable<String>>> pCollectionGroupByKey = MergedListShaik.apply(GroupByKey.create());
+    // Convert to a custom Value object (RankedPageRohit) in preparation for Job 2
+    PCollection<KV<String, RankedPageShaik>> job02Input = pCollectionGroupByKey.apply(ParDo.of(new Job1Finalizer()));
+  
+    PCollection<KV<String,RankedPageShaik>> job2Mapper = job02Input.apply(ParDo.of(new Job2Mapper()));
+  
 
-    PCollection<String> PCLinkString =  MergedListShaik.apply(
+    PCollection<KV<String, RankedPageShaik>> job02Output = null; 
+    PCollection<KV<String,Iterable<RankedPageShaik>>> job02MapperGroupbkey = job2Mapper.apply(GroupByKey.create());
+    
+    job02Output = job02MapperGroupbkey.apply(ParDo.of(new Job2Updater()));
+
+  
+    job02MapperGroupbkey = job02Output.apply(GroupByKey.create());
+    
+    job02Output = job02MapperGroupbkey.apply(ParDo.of(new Job2Updater()));
+  
+    job02Output = job02MapperGroupbkey.apply(ParDo.of(new Job2Updater()));
+    job02MapperGroupbkey = job02Output.apply(GroupByKey.create());    
+    job02Output = job02MapperGroupbkey.apply(ParDo.of(new Job2Updater()));  
+
+    PCollection<String> PCLinkString =  job02Output.apply(
       MapElements.into(  
-        TypeDescriptors.strings())
-          .via((myMergeLstout) -> myMergeLstout.toString()));
+        TypeDescriptors.strings()).via(
+        kvtoString -> kvtoString.toString()));
 
        
         //
